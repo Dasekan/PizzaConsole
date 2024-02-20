6e5fc2ec35ed6e5e52ae99a7220b37b767e37836
Dasek <Dasek@DASE>
2024-02-20 13:03:55 +0100

    Add project files.

---
+++ PizzaConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
